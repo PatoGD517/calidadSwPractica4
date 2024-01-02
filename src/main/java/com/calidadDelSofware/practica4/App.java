aa5690430cf1a032fb918669b7d6f004599b80e8
Diego-Arciniegas <62758837+Diego-Arciniegas@users.noreply.github.com>
2024-01-02 21:05:05 +0100

    Integración Practica 4 con GIT

---
+++ src/main/java/com/calidadDelSofware/practica4/App.java
@@ -0,0 +1,13 @@
+package com.calidadDelSofware.practica4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
